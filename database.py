26edf054e00cf3d6c3406f1569c81c1e0ba25854
IMP XINGWANG <geminifoto0004@gmail.com>
2026-02-09 21:03:11 -0300

    Fix save_id_type format in access tokens table

--- database.py
+++ database.py
@@ -552,6 +552,7 @@ def init_database():
             text_type=get_text_type(),
             text_type_unique=get_text_type_unique(),
             text_type_with_default=get_text_type_with_default(),
+            save_id_type=get_text_type_uuid(),
             timestamp_default=get_timestamp_default()
         ))
         print("users è¡¨å·²å°±ç?")
